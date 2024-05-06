38483dcbd2fb1721767eeede7c620ed9e10e1836
MaximMacarone <maximmacarone@gmail.com>
2024-05-06 22:07:48 +0300

    Initial Commit

---
+++ DelApp/DelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DelAppApp.swift
+//  DelApp
+//
+//  Created by Maxim Makarenkov on 06.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct DelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
